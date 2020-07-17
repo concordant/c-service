828c024326c9a58bd874c275297db70ecbb0298f
Florent Coriat <florent.coriat@inria.fr>
2020-07-17 02:32:47 +0200

    Setup ESlint
    - Configure ESlint
    - npm run lint   set to run ESlint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  rules: {},
+};
